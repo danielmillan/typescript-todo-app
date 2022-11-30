f8e2f377633044ce1d46d2d85c2551d3da2d6871
dirtecnologia-nuawi <dir.tecnologia@nuawi.com>
2022-11-29 21:46:51 -0500

    define oas and set middleware

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,37 @@
-import express from "express";
-import MongoService from "./services/MongoService";
-import SessionMiddleware from "./middlewares/session";
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from "./oas.json";
+import MongoService from './services/MongoService';
+import SessionMiddleware from './middlewares/session';
 // Controllers
-import TodoController from "./controllers/TodoController";
-import UserController from "./controllers/UserController";
-import RoleController from "./controllers/RoleController";
-import authController from "./controllers/authController";
+import TodoController from './controllers/TodoController';
+import UserController from './controllers/UserController';
+import RoleController from './controllers/RoleController';
+import authController from './controllers/authController';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/todo", [SessionMiddleware.validateRouteAuthentication], TodoController);
-app.use("/user", [SessionMiddleware.validateRouteAuthentication], UserController);
-app.use("/role", [SessionMiddleware.validateRouteAuthentication], RoleController);
-app.use("/auth", authController);
+app.use(
+  '/todo',
+  [SessionMiddleware.validateRouteAuthentication],
+  TodoController
+);
+app.use(
+  '/user',
+  [SessionMiddleware.validateRouteAuthentication],
+  UserController
+);
+app.use(
+  '/role',
+  [SessionMiddleware.validateRouteAuthentication],
+  RoleController
+);
+app.use('/auth', authController);
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Start database
 MongoService.connect();
--- src/middlewares/permisions.ts
+++ src/middlewares/permisions.ts
@@ -1,52 +1,25 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 import { ERolePermisions } from '../models/IRole';
 
-export default class PermisionsMiddleware{
-public static validateSearchPermisions(request:Request, response: Response, nextFunction: NextFunction){
-    try {
-        if(response.locals.user.permisions.includes(ERolePermisions.SEARCH)){
-            nextFunction();
-        } else{
-            response.status(401).send({ status: 'Failed', result: 'No tiene acceso a este recurso.' });
-        }
-    } catch (error) {
-        response.status(500).send({ status: "Failed", result: error});
-    }
-}
-
-public static validateCreatePermisions(request: Request, response: Response, nextFunction: NextFunction){
-    try {
-        if(response.locals.user.permisions.includes(ERolePermisions.CREATE)){
-            nextFunction();
-        } else{
-            response.status(401).send({ status: 'Failed', result: 'No tiene acceso a este recurso.' });
-        }
-    } catch (error) {
-        response.status(500).send({ status: "Failed", result: error});
-    }
-}
-
-public static validateDeletePermisions(request: Request, response: Response, nextFunction: NextFunction){
-    try {
-        if(response.locals.user.permisions.includes(ERolePermisions.DELETE)){
-            nextFunction();
-        } else{
-            response.status(401).send({ status: 'Failed', result: 'No tiene acceso a este recurso.' });
-        }
-    } catch (error) {
-        response.status(500).send({ status: "Failed", result: error});
-    }
-}
-
-public static validateEditPermisions(request: Request, response: Response, nextFunction: NextFunction){
-    try {
-        if(response.locals.user.permisions.includes(ERolePermisions.EDIT)){
-            nextFunction();
-        } else{
-            response.status(401).send({ status: 'Failed', result: 'No tiene acceso a este recurso.' });
-        }
-    } catch (error) {
-        response.status(500).send({ status: "Failed", result: error});
-    }
-}
+export default class PermisionsMiddleware {
+  public static validateAction(permission: ERolePermisions) {
+    return function (
+      request: Request,
+      response: Response,
+      nextFunction: NextFunction
+    ) {
+      try {
+        response.locals.user.permisions.find(
+          (p: ERolePermisions) => p === permission
+        )
+          ? nextFunction()
+          : response.status(401).send({
+              status: 'Failed',
+              result: 'No tiene los permisos para acceder a este recurso.',
+            });
+      } catch (error) {
+        response.status(500).send({ status: 'Failed', result: error });
+      }
+    };
+  }
 }
